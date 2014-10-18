6de0e65ec6c6db641e8f6acd4474642768de9628
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2014-10-18 16:16:11 -0400

    update tests for pure prefetch in T8256 to reflect the design changes in #9353

--- testsuite/tests/codeGen/should_run/T8256.hs
+++ testsuite/tests/codeGen/should_run/T8256.hs
@@ -10,38 +10,37 @@ import GHC.ST
 import Data.Primitive.ByteArray
 import Control.Monad.Primitive
 
+import qualified Control.Monad.ST as ST
 
 
-sameByteArray  :: Control.Monad.Primitive.PrimMonad m => ByteArray -> ByteArray -> m Bool
-sameByteArray ar1 ar2 =
+sameByteArray  :: ByteArray -> ByteArray ->  Bool
+sameByteArray ar1 ar2 = runST $
         do  v1 <- unsafeThawByteArray ar1
             v2 <- unsafeThawByteArray ar2
             return $ sameMutableByteArray v1 v2
 
-pf0 (ByteArray by) =  ByteArray ( prefetchByteArray0# by 1#)
-
-pf1 (ByteArray by) =  ByteArray  (prefetchByteArray1# by 1#)
-
-pf2 (ByteArray by) = ByteArray (  prefetchByteArray2# by 1#)
-
-pf3 (ByteArray by) = ByteArray (  prefetchByteArray3# by  1#)
 
 
-monoSame v f = sameByteArray v (f v)
+appBy :: Monad m => (ByteArray# -> Int# -> a -> a) -> ByteArray  -> a -> m a
+appBy f (ByteArray by) a =  f #1 by  a
 
-
-
+--isSameByteArray :: ByteArray -> ByteArray -> Bool
+--isSameByteArray = sameByteArray
 
 main :: IO ()
-main = do  
+main = do
     mv1 <- newByteArray 17
     v1 <- unsafeFreezeByteArray mv1
-    return () 
-    t0<- monoSame v1 pf0 
-    t1 <- monoSame v1 pf1 
-    t2 <- monoSame v1 pf2 
-    t3 <- monoSame v1 pf3
-    if t0 && t1 && t2 && t3  then putStrLn "success" else error "bad prefetch operation! please report" 
+    --return ()
+    t0<- appBy (prefetchByteArray0) v1 v1
+    t1 <- appBy (prefetchByteArray0) v1 v1
+    t2 <- appBy (prefetchByteArray0) v1 v1
+    t3 <- appBy (prefetchByteArray0) v1 v1
+    if t0 `sameByteArray`  v1
+      && t1 `sameByteArray`  v1
+      && t2 `sameByteArray`  v1
+      && t3  `sameByteArray`  v1
+      then putStrLn "success" else error "bad prefetch operation! please report"
 
 
 
